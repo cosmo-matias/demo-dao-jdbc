2ec27e889a4838e62bebed41e46fd61a88b66a28
Cosmo Matias <ducosmotdl@gmail.com>
2022-09-18 18:33:51 -0300

    delete implamentation by DepartmentDaoJDBC

--- src/applicatin/Program2.java
+++ src/applicatin/Program2.java
@@ -1,6 +1,7 @@
 package applicatin;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -10,6 +11,8 @@ public class Program2 {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
 		System.out.println("=== TEST 1: department findById ===");
@@ -33,6 +36,13 @@ public class Program2 {
 		departmentDao.upade(department);
 		System.out.println("Update completed");
 		
+		System.out.println("\n=== TEST 5: department delete ===");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
 
 }

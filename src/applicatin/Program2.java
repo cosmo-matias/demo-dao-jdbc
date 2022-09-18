39785fdfdb508f064d881e5eddfab27c15330887
Cosmo Matias <ducosmotdl@gmail.com>
2022-09-18 16:49:12 -0300

    findAll implementation by DepartmentDaoJDBC

--- src/applicatin/Program2.java
+++ src/applicatin/Program2.java
@@ -1,18 +1,28 @@
 package applicatin;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program2 {
 
 	public static void main(String[] args) {
 
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-		
+
 		System.out.println("=== TEST 1: department findById ===");
 		Department department = departmentDao.findById(2);
 		System.out.println(department);
+
+		System.out.println("\n=== TEST 2: department findAll ===");
+		List<Department> list = departmentDao.findAll();
+		for (Department obj : list) {
+			System.out.println(obj);
+		}
+
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -11,30 +12,30 @@ import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
-public class DepartmentDaoJDBC implements DepartmentDao{
+public class DepartmentDaoJDBC implements DepartmentDao {
 
 	private Connection conn;
 
 	public DepartmentDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	@Override
 	public void insart(Department obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void upade(Department obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -69,8 +70,25 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		PreparedStatement st = null;
+		ResultSet rs = null;
 
+		try {
+			st = conn.prepareStatement("SELECT * FROM department ORDER BY Name");
+			rs = st.executeQuery();
+
+			List<Department> list = new ArrayList<>();
+
+			while (rs.next()) {
+				Department dep = instantiateDepartment(rs);
+				list.add(dep);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
 }
